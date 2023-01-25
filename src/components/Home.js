8ad2b62d4d85b50757936a3c38b1676d354f2aa5
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-01-25 22:12:03 +0200

    Three random blog-entries have been added; use state as a hook was used, along with props.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "Trip to Thailand",
+      body: "lorem ipsum...",
+      author: "Mario",
+      id: 1,
+    },
+
+    {
+      title: "One week in Greece",
+      body: "lorem ipsum...",
+      author: "Gabriel",
+      id: 2,
+    },
+
+    {
+      title: "Cristal water in Spain.",
+      body: "lorem ipsum...",
+      author: "George",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      <BlogList blogs={blogs} title="All blogs!" />
     </div>
   );
 };
