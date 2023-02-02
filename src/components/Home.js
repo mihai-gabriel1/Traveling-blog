ec975311e8cc44ad0e288eba3f9ab63608249a60
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-02-02 21:47:17 +0200

    A conditional loading message has been added, that will be displayed untill the data is fetched from the local api.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -14,18 +15,21 @@ const Home = () => {
   };
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div className="loading-message">Loading...</div>}
       {blogs && (
         <BlogList
           blogs={blogs}
