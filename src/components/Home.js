c390ec67896d2cf29c784f901c57308a4365c2ff
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-02-05 11:07:11 +0200

    The errors of any kind in what regards fetching will now be displayed into the browser; however, if you preview this project from the live link, you will get the fetching error message, because the data is contained in the database on the json server, so prior to that, make sure you run the json server so you can fetch the data succesfully.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -17,19 +18,25 @@ const Home = () => {
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource.");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
         setIsPending(false);
+        setError(null);
       })
       .catch((err) => {
-        console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div> {error} </div>}
       {isPending && <div className="loading-message">Loading...</div>}
       {blogs && (
         <BlogList
