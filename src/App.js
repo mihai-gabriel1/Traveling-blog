ee4389bf67ce6293bfaab9879921496bc44113a0
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-02-05 21:25:47 +0200

    To get rid of the requests sent to the server every time we click on a link or component, react router has been implemented.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Create from "./Create";
 
 function App() {
   return (
-    <div className="App">
-      <div className="nav-section">
-        <Navbar />
+    <Router>
+      <div className="App">
+        <div className="nav-section">
+          <Navbar />
+        </div>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
+        <div className="footer-section">
+          <Footer />
+        </div>
       </div>
-      <div className="content">
-        <Home />
-      </div>
-      <div className="footer-section">
-        <Footer />
-      </div>
-    </div>
+    </Router>
   );
 }
 
