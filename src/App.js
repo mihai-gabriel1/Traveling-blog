81fb3dd3624f68f9266361552185666dce80ff57
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-02-09 22:32:09 +0200

    Router parameter have been used, in order to be able to acces and display the certain data, if there is any, of our blog entries; the underline created by the Links has been removed.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
+import BlogDetails from "./components/BlogDetails";
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
 
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,10 +1,14 @@
-const BlogList = ({ blogs, title, handleDelete, handleDeleteAll }) => {
+import { Link } from "react-router-dom";
+
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2> {title} </h2>
       {blogs.map((blog) => (
         <div className="blog-preview">
-          <h2> {blog.title} </h2>
+          <Link to={`/blogs/${blog.id}/`}>
+            <h2> {blog.title} </h2>
+          </Link>
           <p> Written by {blog.author} </p>
         </div>
       ))}
