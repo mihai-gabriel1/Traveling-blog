6d6515a8c827a1159cd1c12cda72335116065e97
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-02-11 21:50:51 +0200

    A github logo has been added to the footer, that links to my github profile.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 const Footer = () => {
+  const githubLogo = (
+    <a href="https://github.com/mihai-gabriel1" target="_blank">
+      <img
+        src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+        alt="github.com logo"
+        className="github-logo"
+      />
+    </a>
+  );
+
   return (
     <div className="footer">
       <p>Copyright by Mihai Gabriel</p>
+      {githubLogo}
     </div>
   );
 };
